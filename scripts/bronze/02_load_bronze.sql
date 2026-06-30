3bba3a8b509489fac4d5e27ef050ce88e47eda96
Adham Eltantawi <adhameltantawi@gmail.com>
2026-06-30 22:23:16 +0300

    Update 02_load_bronze.sql

--- scripts/bronze/02_load_bronze.sql
+++ scripts/bronze/02_load_bronze.sql
@@ -25,3 +25,25 @@ WITH
 );
 GO
 
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'E:\data\braa\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
+WITH    
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+GO
+
+TRUNCATE TABLE bronze.erp_custaz12;
+BULK INSERT bronze.erp_custaz12
+FROM 'E:\data\braa\sql-data-warehouse-project-main\datasets\source_crm\erp_custaz12'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+GO
+
