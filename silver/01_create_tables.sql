efcc1df5478d3cdd021fd39fd78a36c78b2f2b84
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-01 18:25:02 +0300

    add 'dwh_create_date'

--- silver/01_create_tables.sql
+++ silver/01_create_tables.sql
@@ -13,7 +13,8 @@ CREATE TABLE silver.crm_cust_info
      cst_lastname       NVARCHAR(50),
      cst_marital_status NVARCHAR(50),
      cst_gndr           NVARCHAR(50),
-     cst_create_date    DATE
+     cst_create_date    DATE,
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO
@@ -29,7 +30,8 @@ CREATE TABLE silver.crm_prd_info
      prd_cost     INT,
      prd_line     NVARCHAR(50),
      prd_start_dt DATETIME,
-     prd_end_dt   DATETIME
+     prd_end_dt   DATETIME,
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO
@@ -47,7 +49,8 @@ CREATE TABLE silver.crm_sales_details
      sls_due_dt   INT,
      sls_sales    INT,
      sls_quantity INT,
-     sls_price    INT
+     sls_price    INT,
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO
@@ -59,7 +62,8 @@ CREATE TABLE silver.erp_custaz12
   (
      cid   NVARCHAR(50),
      bdate DATE,
-     gen   NVARCHAR(50)
+     gen   NVARCHAR(50),
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO
@@ -70,7 +74,8 @@ IF Object_id('silver.erp_loc_a101', 'U') IS NOT NULL
 CREATE TABLE silver.erp_loc_a101
   (
      cid   NVARCHAR(50),
-     cntry NVARCHAR(50)
+     cntry NVARCHAR(50),
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO
@@ -83,7 +88,8 @@ CREATE TABLE silver.erp_px_cat_g1v2
      id          NVARCHAR(50),
      cat         NVARCHAR(50),
      subcat      NVARCHAR(50),
-     maintenance NVARCHAR(50)
+     maintenance NVARCHAR(50),
+     dwh_create_date    DATETIME2 DEFAULT GETDATE()
   );
 
 GO 
