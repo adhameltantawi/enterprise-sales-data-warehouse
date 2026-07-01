c0f2b0645a3daa75195fe31e6d12d1192c91db02
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-01 09:15:48 +0300

    add Script Purpose

--- scripts/bronze/02_load_bronze.sql
+++ scripts/bronze/02_load_bronze.sql
@@ -1,3 +1,26 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
+    It performs the following actions:
+
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from CSV files to bronze tables.
+
+Parameters:
+    None.
+    This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+
+    EXEC bronze.load_bronze;
+
+===============================================================================
+*/
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
     DECLARE @start_time DATETIME2, @end_time DATETIME2, @batch_start_time DATETIME2, @batch_end_time DATETIME2;
@@ -164,7 +187,7 @@ BEGIN
     PRINT 'Loading Bronze Layer is Completed';    
     PRINT '   - total loading duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR(50)) + ' seconds';
     PRINT '==========================================';
-    
+
     END TRY
     BEGIN CATCH
         PRINT '==========================================';
